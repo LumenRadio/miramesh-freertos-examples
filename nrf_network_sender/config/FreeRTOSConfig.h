37aea247fa334cf5c4f72fd3fd66c815ff05ace5
Sebastian Andersson <sebastian@bittr.nu>
2022-12-19 16:16:33 +0100

    Fix configMAX_PRIORITIES

--- nrf_network_sender/config/FreeRTOSConfig.h
+++ nrf_network_sender/config/FreeRTOSConfig.h
@@ -63,7 +63,7 @@
         SystemCoreClock)
 #define configTICK_RATE_HZ \
     1000
-#define configMAX_PRIORITIES                                                      (3)
+#define configMAX_PRIORITIES                                                      (4)
 #define configMINIMAL_STACK_SIZE                                                  ( \
         60)
 #define configTOTAL_HEAP_SIZE                                                     ( \
